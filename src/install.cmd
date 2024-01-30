f81e2430bbcad47c523436ab53889aeeea075417
Maurice Lim <mau.mauricelim@gmail.com>
2024-01-30 15:43:32 +0800

    Initial working install

--- src/install.cmd
+++ src/install.cmd
@@ -1,4 +1,19 @@
 @echo off
-@SET distr=ubuntu-latest
+@SET distro_name=ubuntu-latest
 @SET dir=%cd%
-@SET AREYOUSURE=N
+@SET CONFIRM=n
+@echo Importing WSL distribution: [%distro_name%]
+@echo from current directory: [%dir%]
+
+setlocal
+@echo(
+@echo This action will overwrite any existing WSL distribution with the same name: [%distro_name%].
+:PROMPT
+SET /P CONFIRM=Do you want to continue? [Y/n]
+IF /I "%CONFIRM%" NEQ "Y" GOTO END
+
+@powershell -Command "wsl --import %distro_name% '%USERPROFILE%\AppData\Local\Packages' .\zsh-nvim.tar.gz"
+
+:END
+endlocal
+@pause
