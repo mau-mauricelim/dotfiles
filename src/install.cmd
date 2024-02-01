947112548e23f8a2ae99afefa7431cbc0c537372
Maurice Lim <mau.mauricelim@gmail.com>
2024-02-01 23:12:56 +0800

    Add trailing whitespaces for prompt

--- src/install.cmd
+++ src/install.cmd
@@ -9,14 +9,14 @@ setlocal
 @echo This action will overwrite any existing WSL distribution with the same name: [%distro_name%].
 @SET CONFIRM=n
 :PROMPT
-SET /P CONFIRM=Do you want to continue? [Y/n]
+SET /P CONFIRM=Do you want to continue? [Y/n] 
 IF /I "%CONFIRM%" NEQ "Y" GOTO END
 @powershell -Command "if((wsl -l -v | Where {$_.Replace(\"`0\",\"\") -match \" %distro_name% \"}).count -gt 0) {wsl --unregister %distro_name%}; $InstallLocation='%USERPROFILE%\AppData\Local\Packages\WslCustomDistro\%distro_name%'; if(-not(Test-Path -PathType container $InstallLocation)) {$null = new-item $InstallLocation -ItemType Directory}; wsl --import %distro_name% $InstallLocation $(ls .\*.tar.gz)"
 
 @echo(
 @SET CONFIRM=n
 :PROMPT
-SET /P CONFIRM=Set WSL distribution [%distro_name%] as default? [Y/n]
+SET /P CONFIRM=Set WSL distribution [%distro_name%] as default? [Y/n] 
 IF /I "%CONFIRM%" NEQ "Y" GOTO END
 @powershell -Command "wsl -s %distro_name%"
 
