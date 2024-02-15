829966ba45796ee838d853ea9ac15bde97a17c44
Maurice Lim <mau.mauricelim@gmail.com>
2024-02-15 23:06:39 +0800

    update completion zsh

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -1,23 +1,43 @@
 # Enabling the Zsh Completion System
 autoload -Uz compinit
 compinit
+_comp_options+=(globdots)   # Show hidden files
 
-# Configuring Zsh Completion With zstyle
-zstyle ':completion:*' auto-description 'specify: %d'
-zstyle ':completion:*' completer _expand _complete _correct _approximate
-zstyle ':completion:*' format 'Completing %d'
-zstyle ':completion:*' group-name ''
-# The Completion Menu
+## Options
+setopt GLOB_COMPLETE        # Show autocompletion menu with globs
+setopt MENU_COMPLETE        # Automatically highlight first element of completion menu
+setopt AUTO_LIST            # Automatically list choices on ambiguous completion.
+setopt COMPLETE_IN_WORD     # Complete from both ends of a word.
+
+## Zstyles
+# zstyle pattern
+# :completion:<function>:<completer>:<command>:<argument>:<tag>
+
+# Define the completer
+zstyle ':completion:*' completer _extensions _complete _approximate
+# Enable cache
+zstyle ':completion:*' use-cache on
+zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
+
+# Completion menu
 zstyle ':completion:*' menu select
-eval "$(dircolors -b)"
-zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*' list-colors ''
-zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
-zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
-zstyle ':completion:*' menu select=long
-zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
-zstyle ':completion:*' use-compctl false
-zstyle ':completion:*' verbose true
+zstyle ':completion:*' squeeze-slashes true
+zstyle ':completion:*' keep-prefix true
 
-zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
-zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+# Display format
+zstyle ':completion:*'                   format '%F{yellow}-- %d --%f'
+zstyle ':completion:*:*:*:*:corrections' format '%F{red}-- %d (errors %e) --%f'
+zstyle ':completion:*:*:*:*:warnings'    format '%F{red}-- No matches found --%f'
+# Colors for files and directories
+eval "$(dircolors -b)"
+zstyle ':completion:*:*:*:*:default' list-colors ${(s.:.)LS_COLORS}
+# Only display certain tags for the command cd
+zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
+
+# Group results
+zstyle ':completion:*' group-name ''
+# Modify the order of descriptions displayed
+zstyle ':completion:*:*:-command-:*:*' group-order aliases builtins functions commands
+
+# See ZSHCOMPWID "completion matching control"
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
