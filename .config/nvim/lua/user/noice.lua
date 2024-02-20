a14e9ecd77abb5b7a9989894c905e14d153f9d32
Maurice Lim <mau.mauricelim@gmail.com>
2024-02-20 19:53:22 +0800

    Update noice plugin and remove Dockerfile no longer needed

--- .config/nvim/lua/user/noice.lua
+++ .config/nvim/lua/user/noice.lua
@@ -1,11 +1,4 @@
 -- Dismiss Noice Message
 vim.keymap.set("n", "<leader>nd", "<cmd>NoiceDismiss<CR>", {desc = "Dismiss Noice Message"})
 
-require("noice").setup({
-  routes = {
-    {
-      view = "split",
-      filter = { event = "msg_show", min_height = 1 },
-    },
-  },
-})
+require("noice").setup()
