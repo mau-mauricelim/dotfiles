9ee7488708ac56150f42c982cf18d74ff341e147
Maurice Lim <mau.mauricelim@gmail.com>
2024-05-30 08:59:27 +0000

    update test and build

--- test/version.sh
+++ test/version.sh
@@ -7,53 +7,53 @@ latestAndInstalledVersion() {
 }
 
 latestAndInstalledVersion zsh \
-    "$(curl -s 'https://zsh.sourceforge.io/FAQ/zshfaq01.html' | grep 'latest production version' | cut -d' ' -f2)" \
+    "$(curl -s https://zsh.sourceforge.io/FAQ/zshfaq01.html|grep 'latest production version'|cut -d' ' -f2)" \
     "$(zsh --version)"
 
-latestAndInstalledVersion rg \
-    "$(curl -s 'https://api.github.com/repos/BurntSushi/ripgrep/releases/latest' | grep tag_name | cut -d '"' -f4)" \
-    "$(rg --version | grep ripgrep)"
-
-latestAndInstalledVersion fd \
-    "$(curl -s 'https://api.github.com/repos/sharkdp/fd/releases/latest' | grep tag_name | cut -d '"' -f4)" \
-    "$(fd --version)"
-
-latestAndInstalledVersion bat \
-    "$(curl -s 'https://api.github.com/repos/sharkdp/bat/releases/latest' | grep tag_name | cut -d '"' -f4)" \
-    "$(bat --version)"
-
 latestAndInstalledVersion git \
-    "$(curl -s 'https://api.github.com/repos/git/git/tags' | grep name | head -1 | cut -d '"' -f4)" \
+    "$(curl -s https://github.com/git/git/tags|grep 'Link--primary Link'|head -1|sed 's/.*class="Link--primary Link">\([^<]*\).*/\1/')" \
     "$(git --version)"
 
 latestAndInstalledVersion stow \
-    "$(curl -s 'https://api.github.com/repos/aspiers/stow/tags' | grep name | head -1 | cut -d '"' -f4)" \
+    "$(curl -s https://github.com/aspiers/stow/tags|grep 'Link--primary Link'|head -1|sed 's/.*class="Link--primary Link">\([^<]*\).*/\1/')" \
     "$(stow --version)"
 
+latestAndInstalledVersion rg \
+    "$(curl -sL https://github.com/BurntSushi/ripgrep/releases/latest|grep 'breadcrumb-item-selected'|rev|cut -d'>' -f1|rev|xargs)" \
+    "$(rg --version|grep ripgrep)"
+
+latestAndInstalledVersion fd \
+    "$(curl -sL https://github.com/sharkdp/fd/releases/latest|grep 'breadcrumb-item-selected'|rev|cut -d'>' -f1|rev|xargs)" \
+    "$(fd --version)"
+
+latestAndInstalledVersion bat \
+    "$(curl -sL https://github.com/sharkdp/bat/releases/latest|grep 'breadcrumb-item-selected'|rev|cut -d'>' -f1|rev|xargs)" \
+    "$(bat --version)"
+
 latestAndInstalledVersion tmux \
-    "$(curl -s 'https://api.github.com/repos/tmux/tmux/releases/latest' | grep tag_name | cut -d '"' -f4)" \
+    "$(curl -sL https://github.com/tmux/tmux/releases/latest|grep 'breadcrumb-item-selected'|rev|cut -d'>' -f1|rev|xargs)" \
     "$(tmux -V)"
 
 latestAndInstalledVersion eza \
-    "$(curl -s 'https://api.github.com/repos/eza-community/eza/releases/latest' | grep tag_name | cut -d '"' -f4)" \
-    "$(eza -v | grep ^v)"
+    "$(curl -sL https://github.com/eza-community/eza/releases/latest|grep 'breadcrumb-item-selected'|rev|cut -d'>' -f1|rev|xargs)" \
+    "$(eza -v|grep ^v)"
 
 latestAndInstalledVersion lazygit \
-    "$(curl -s 'https://api.github.com/repos/jesseduffield/lazygit/releases/latest' | grep tag_name | cut -d '"' -f4)" \
-    "$(lazygit --version | sed 's/.*, version=\([^ ]*\),.*$/\1/')"
+    "$(curl -sL https://github.com/jesseduffield/lazygit/releases/latest|grep 'breadcrumb-item-selected'|rev|cut -d'>' -f1|rev|xargs)" \
+    "$(lazygit --version|sed 's/.*, version=\([^ ]*\),.*$/\1/'|head -1)"
 
 latestAndInstalledVersion delta \
-    "$(curl -s 'https://api.github.com/repos/dandavison/delta/releases/latest' | grep tag_name | cut -d '"' -f4)" \
-    "$(delta --version)"
+    "$(curl -sL https://github.com/dandavison/delta/releases/latest|grep 'breadcrumb-item-selected'|rev|cut -d'>' -f1|rev|xargs)" \
+    "$(delta --version|head -1)"
 
 latestAndInstalledVersion zoxide \
-    "$(curl -s 'https://api.github.com/repos/ajeetdsouza/zoxide/releases/latest' | grep tag_name | cut -d '"' -f4)" \
+    "$(curl -sL https://github.com/ajeetdsouza/zoxide/releases/latest|grep 'breadcrumb-item-selected'|rev|cut -d'>' -f1|rev|xargs)" \
     "$(zoxide --version)"
 
 latestAndInstalledVersion nvim \
-    "$(curl -s 'https://api.github.com/repos/nvim/nvim/releases/latest' | grep tag_name | cut -d '"' -f4)" \
-    "$(nvim --version | grep NVIM)"
+    "$(curl -sL https://github.com/nvim/nvim/releases/latest|grep 'breadcrumb-item-selected'|rev|cut -d'>' -f1|rev|xargs)" \
+    "$(nvim --version|grep NVIM)"
 
 latestAndInstalledVersion fzf \
-    "$(curl -s 'https://api.github.com/repos/junegunn/fzf/releases/latest' | grep tag_name | cut -d '"' -f4)" \
+    "$(curl -sL https://github.com/junegunn/fzf/releases/latest|grep 'breadcrumb-item-selected'|rev|cut -d'>' -f1|rev|xargs)" \
     "$(fzf --version)"
