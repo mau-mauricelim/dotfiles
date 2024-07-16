b1a729045fc68caeb143356352ff3358bf1731db
Maurice Lim <mau.mauricelim@gmail.com>
2024-07-16 09:30:30 +0000

    Update WhichKey mappings spec - changed in v3

--- .config/nvim/lua/plugin/setup/which-key.lua
+++ .config/nvim/lua/plugin/setup/which-key.lua
@@ -17,26 +17,27 @@ return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
   event = 'VeryLazy',
   config = function() -- This is the function that runs, AFTER loading
-    require('which-key').setup()
+    local wk = require("which-key")
     -- Document existing key chains
     -- stylua: ignore
-    require('which-key').register({
-      ['<Leader>c'] = { name = '[C]ode',              _ = 'which_key_ignore' },
-      ['<Leader>d'] = { name = '[D]ocument',          _ = 'which_key_ignore' },
-      ['<Leader>r'] = { name = '[R]ename',            _ = 'which_key_ignore' },
-      ['<Leader>s'] = { name = '[S]earch',            _ = 'which_key_ignore' },
-      ['<Leader>w'] = { name = '[W]orkspace',         _ = 'which_key_ignore' },
-      ['<Leader>/'] = { name = '[/]search',           _ = 'which_key_ignore' },
-      ['<Leader>b'] = { name = '[B]uffer',            _ = 'which_key_ignore' },
-      ['<Leader>f'] = { name = '[F]ormat',            _ = 'which_key_ignore' },
-      ['<Leader>g'] = { name = '[G]it',               _ = 'which_key_ignore' },
-      ['<Leader>h'] = { name = '[H]unk',              _ = 'which_key_ignore' },
-      ['<Leader>l'] = { name = '[L]azy*, Lua, Lines', _ = 'which_key_ignore' },
-      ['<Leader>n'] = { name = '[N]oice',             _ = 'which_key_ignore' },
-      ['<Leader>p'] = { name = '[P]ersistence',       _ = 'which_key_ignore' },
-      ['<Leader>t'] = { name = '[T]oggle',            _ = 'which_key_ignore' },
-      ['<Leader>v'] = { name = '[V]irtual',           _ = 'which_key_ignore' },
-      ['<Leader>x'] = { name = 'E[X]ecute',           _ = 'which_key_ignore' },
+    -- require('which-key').register({
+    wk.add({
+      { '<Leader>c', group = '[C]ode' },
+      { '<Leader>d', group = '[D]ocument' },
+      { '<Leader>r', group = '[R]ename' },
+      { '<Leader>s', group = '[S]earch' },
+      { '<Leader>w', group = '[W]orkspace' },
+      { '<Leader>/', group = '[/]search' },
+      { '<Leader>b', group = '[B]uffer' },
+      { '<Leader>f', group = '[F]ormat' },
+      { '<Leader>g', group = '[G]it' },
+      { '<Leader>h', group = '[H]unk' },
+      { '<Leader>l', group = '[L]azy*' },
+      { '<Leader>n', group = '[N]oice' },
+      { '<Leader>p', group = '[P]ersistence' },
+      { '<Leader>t', group = '[T]oggle' },
+      { '<Leader>v', group = '[V]irtual' },
+      { '<Leader>x', group = 'E[X]ecute' },
     })
   end,
 }
