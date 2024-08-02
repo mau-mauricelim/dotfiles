6f536e6d2967d9c3e008f000b968d1e30d7f4bcc
Maurice Lim <mau.mauricelim@gmail.com>
2024-08-02 14:04:51 +0000

    Fix conflicting keymap with hunk

--- .config/nvim/lua/plugin/setup/harpoon.lua
+++ .config/nvim/lua/plugin/setup/harpoon.lua
@@ -13,8 +13,8 @@ return {
     vim.keymap.set('n', '<Leader>a', function() harpoon:list():add(); vim.notify('Added ' .. vim.fn.expand('%') .. ' to harpoon list') end, { desc = '[A]dd file to harpoon list' })
     vim.keymap.set('n', '<C-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = 'Toggle harpoon list' })
     -- Toggle previous & next buffers stored within Harpoon list
-    vim.keymap.set('n', '[h', function() harpoon:list():prev() end, { desc = 'Next [H]arpoon buffer' })
-    vim.keymap.set('n', ']h', function() harpoon:list():next() end, { desc = 'Previous [H]arpoon buffer' })
+    vim.keymap.set('n', '[a', function() harpoon:list():prev() end, { desc = 'Next H[a]rpoon buffer' })
+    vim.keymap.set('n', ']a', function() harpoon:list():next() end, { desc = 'Previous H[a]rpoon buffer' })
 
     -- Basic telescope configuration
     local conf = require('telescope.config').values
