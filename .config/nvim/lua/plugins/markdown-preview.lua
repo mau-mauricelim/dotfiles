89254e961ae0c3f2a65646e490928075dca2f915
Maurice Lim <mau.mauricelim@gmail.com>
2024-08-12 13:54:59 +0000

    Fix bug

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -6,10 +6,8 @@ return {
     { '<Leader>tm', '<cmd>MarkdownPreviewToggle<CR>', desc = 'Toggle [M]arkdown preview' },
   },
   ft = { 'markdown' },
-  -- BUG: Vim:E117: Unknown function: mkdp#util#install when install by lazy.nvim
-  -- https://github.com/iamcco/markdown-preview.nvim/issues/690
-  -- If build fail, manually run `:call mkdp#util#install()`
   build = function()
+    require('lazy').load({ plugins = 'markdown-preview.nvim' })
     vim.fn['mkdp#util#install']()
   end,
 }
