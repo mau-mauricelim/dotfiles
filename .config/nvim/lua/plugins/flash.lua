db20fe5bd1a7b386e84454da3e80361786e606f2
Maurice Lim <mau.mauricelim@gmail.com>
2024-09-19 05:22:19 +0000

    Saner flash search with ;,

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -1,8 +1,32 @@
 return {
   'folke/flash.nvim',
   event = 'VeryLazy',
-  ---@type Flash.Config
-  opts = {},
+  config = function()
+    local flash = require('flash')
+    flash.setup({
+      modes = {
+        -- options used when flash is activated through
+        -- `f`, `F`, `t`, `T`, `;` and `,` motions
+        char = {
+          ---@alias Flash.CharActions table<string, "next" | "prev" | "right" | "left">
+          -- The direction for `prev` and `next` is determined by the motion.
+          -- `left` and `right` are always left and right.
+          char_actions = function(motion)
+            return {
+              [';'] = 'right', -- set to `right` to always go right
+              [','] = 'left',  -- set to `left` to always go left
+              -- clever-f style
+              [motion:lower()] = 'next',
+              [motion:upper()] = 'prev',
+              -- jump2d style: same case goes next, opposite case goes prev
+              -- [motion] = "next",
+              -- [motion:match("%l") and motion:upper() or motion:lower()] = "prev",
+            }
+          end,
+        },
+      },
+    })
+  end,
   -- stylua: ignore
   keys = {
     { '<C-f>', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'Flash' },
