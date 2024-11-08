989da9f422d67fd55750495f9b68643d04d4280e
Maurice Lim <mau.mauricelim@gmail.com>
2024-11-08 09:12:59 +0000

    Update lazygit mapping

--- .config/nvim/lua/plugins/lazygit.lua
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -14,6 +14,8 @@ return {
   -- Setting the keybinding for LazyGit with 'keys' is recommended in
   -- order to load the plugin when the command is run for the first time
   keys = {
-     { '<Leader>lg', '<cmd>LazyGit<CR>', desc = 'LazyGit' }
+    { '<Leader>lg', '<cmd>LazyGit<CR>', desc = 'LazyGit' },
+    -- { '<Leader>gl', '<cmd>LazyGit<CR>1a<CR>', desc = '[G]it [L]og all branch' },
+    { '<Leader>gl', '<cmd>LazyGit<CR>4++<CR>', desc = '[G]it [L]og current branch' },
   },
 }
