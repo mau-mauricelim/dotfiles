3ca1c1ae68c19a485f6de70fba10dd9ec212fbbd
Maurice Lim <mau.mauricelim@gmail.com>
2025-01-21 02:38:44 +0000

    Disable less used mappings

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -4,7 +4,7 @@ return { -- "gc" to comment visual regions/lines
   config = function()
     require('Comment').setup()
     -- Map Ctrl-/
-    vim.keymap.set('n', '<C-_>', 'gcc', { desc = 'Comment with Line Comment', remap = true })
-    vim.keymap.set('v', '<C-_>', 'gc', { desc = 'Comment with Line Comment', remap = true })
+    -- vim.keymap.set('n', '<C-_>', 'gcc', { desc = 'Comment with Line Comment', remap = true })
+    -- vim.keymap.set('v', '<C-_>', 'gc', { desc = 'Comment with Line Comment', remap = true })
   end,
 }
