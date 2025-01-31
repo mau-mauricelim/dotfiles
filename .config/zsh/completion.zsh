e50c0263b6923ad7e90fbe5e8f8d1c94984eb73f
mau-mauricelim <77717132+mau-mauricelim@users.noreply.github.com>
2025-01-31 14:37:57 +0800

    Fix bindkey

--- .config/zsh/completion.zsh
+++ .config/zsh/completion.zsh
@@ -51,6 +51,6 @@ zmodload -i zsh/complist
 bindkey -M menuselect '^i' .expand-or-complete-prefix
 # vi navigation keys
 bindkey -M menuselect 'h' vi-backward-char
-bindkey -M menuselect 'j' vi-up-line-or-history
-bindkey -M menuselect 'k' vi-forward-char
-bindkey -M menuselect 'l' vi-down-line-or-history
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
