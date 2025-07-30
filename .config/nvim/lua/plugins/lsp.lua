b3fefd3a69e6afc469b36ea44d862146695ec80f
Maurice Lim <mau.mauricelim@gmail.com>
2025-07-30 01:58:04 +0000

    Update mason to V2

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -2,13 +2,11 @@ return { -- LSP Configuration & Plugins
   'neovim/nvim-lspconfig',
   dependencies = {
     -- Automatically install LSPs and related tools to stdpath for neovim
-    -- Mason must be loaded before its dependents so we need to set it up here.
-    -- BUG: Breaking changes: https://github.com/mason-org/mason-lspconfig.nvim/issues/545
-    { 'mason-org/mason.nvim', opts = {}, version = '^1.0.0' },
-    { 'mason-org/mason-lspconfig.nvim', version = '^1.0.0' },
+    -- NOTE: Mason must be loaded before its dependents so we need to set it up here
+    { 'mason-org/mason.nvim', opts = {} },
+    { 'mason-org/mason-lspconfig.nvim' },
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     'saghen/blink.cmp',
-
     -- Useful status updates for LSP.
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
     {
@@ -21,10 +19,6 @@ return { -- LSP Configuration & Plugins
     },
   },
   config = function()
-    -- Brief Aside: **What is LSP?**
-    --
-    -- LSP is an acronym you've probably heard, but might not understand what it is.
-    --
     -- LSP stands for Language Server Protocol. It's a protocol that helps editors
     -- and language tooling communicate in a standardized fashion.
     --
@@ -135,19 +129,13 @@ return { -- LSP Configuration & Plugins
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      -- clangd = {},
-      -- gopls = {},
-      -- pyright = {},
-      -- rust_analyzer = {},
-      -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+      -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
       -- Some languages (like typescript) have entire language plugins that can be useful:
       --    https://github.com/pmizio/typescript-tools.nvim
       --
-      -- But for many setups, the LSP (`tsserver`) will work just fine
-      -- tsserver = {},
-      --
-
+      -- But for many setups, the LSP (`pyright`) will work just fine
+      pyright = {},
       lua_ls = {
         -- cmd = {...},
         -- filetypes { ...},
@@ -165,6 +153,11 @@ return { -- LSP Configuration & Plugins
         },
       },
     }
+
+    -- Disable LSP progress notifications
+    -- BUG: Since mason-lspconfig breaking change in V2, lua_ls window.progressBar is not working
+    --      https://github.com/mason-org/mason-lspconfig.nvim/issues/545
+    vim.lsp.handlers["$/progress"] = function() end
 
     -- Ensure the servers and tools above are installed
     --  To check the current status of installed tools and/or manually install
@@ -191,7 +184,7 @@ return { -- LSP Configuration & Plugins
 
     require('mason-lspconfig').setup({
       ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-      automatic_installation = false,
+      automatic_enable = true,
       handlers = {
         function(server_name)
           local server = servers[server_name] or {}
