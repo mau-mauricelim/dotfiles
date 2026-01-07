2471096377fbe9032aa69c89fadae0bdc40ff1cf
Maurice Lim <mau.mauricelim@gmail.com>
2026-01-07 03:17:20 +0000

    Revert "Fix treesitter error?"
    This reverts commit 2aebaecec41a50392978387b967d403e1c3bcc84.

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,15 +1,26 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  opts = {
-    ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc' },
-    -- Autoinstall languages that are not installed
-    sync_install = true,
-    auto_install = true,
-    highlight = { enable = true },
-    indent = { enable = true },
-    -- List of parsers to ignore installing (or "all")
-    -- ISSUE: dockerfile parser getting unknown errors
-    ignore_install = { 'dockerfile' },
-  },
+  config = function()
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+
+    ---@diagnostic disable-next-line: missing-fields
+    require('nvim-treesitter.configs').setup({
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc' },
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      -- List of parsers to ignore installing (or "all")
+      -- ISSUE: dockerfile parser getting unknown errors
+      ignore_install = { 'dockerfile' },
+    })
+
+    -- There are additional nvim-treesitter modules that you can use to interact
+    -- with nvim-treesitter. You should go explore a few and see what interests you:
+    --
+    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  end,
 }
