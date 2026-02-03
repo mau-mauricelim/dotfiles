2b948fa9b6fc71abd0c8f1fabcfcfc1e8bd97a70
Maurice Lim <mau.mauricelim@gmail.com>
2026-02-03 02:43:06 +0000

    Update copyparty install script

--- .config/copyparty/install.bat
+++ .config/copyparty/install.bat
@@ -1,14 +1,28 @@
 @echo off
-goto comment
-Install/update copyparty version
 
+goto comment
+Install/update Windows copyparty and cloudflared
 Place this script in a directory (recommended: C:\copyparty)
 And add this directory to PATH
 :comment
 
 set dir=C:\copyparty
-set url=https://github.com/9001/copyparty/releases/latest/download/copyparty-sfx.py
-set out=%dir%\party.py
+md %dir% 2>nul
 
-md %dir%
-curl -sL %url% -o %out%
+call :download "https://github.com/9001/copyparty/releases/latest/download/copyparty-sfx.py" "%dir%\party.py"
+call :download "https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-windows-amd64.exe" "%dir%\cloudflared.exe"
+
+:: pause
+
+:: Exit the script
+exit /b
+
+:download
+set url=%~1
+set out=%~2
+echo Downloading %url% to %out% ...
+curl -sL "%url%" -o "%out%"
+echo Download complete!
+echo.
+:: Exit the function
+exit /b
