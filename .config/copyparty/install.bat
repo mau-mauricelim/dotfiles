9505b0bef4526cbaf4ee461f8742ec571eab56a6
Maurice Lim <mau.mauricelim@gmail.com>
2026-02-10 02:57:24 +0000

    Add icons

--- .config/copyparty/install.bat
+++ .config/copyparty/install.bat
@@ -16,6 +16,7 @@ if "%dir:~-1%"=="\" set "dir=%dir:~0,-1%"
 echo Install path: %dir%
 echo.
 md %dir% 2>nul
+md %dir%\icons 2>nul
 
 call :download "https://raw.githubusercontent.com/mau-mauricelim/dotfiles/main/.config/copyparty/cloudparty.bat" "%dir%\cloudparty.bat"
 call :download "https://raw.githubusercontent.com/mau-mauricelim/dotfiles/main/.config/copyparty/cloudparty.ps1" "%dir%\cloudparty.ps1"
@@ -25,21 +26,23 @@ call :download "https://raw.githubusercontent.com/mau-mauricelim/dotfiles/main/.
 call :download "https://raw.githubusercontent.com/mau-mauricelim/dotfiles/main/.config/copyparty/READMD.md" "%dir%\READMD.md"
 call :download "https://raw.githubusercontent.com/mau-mauricelim/dotfiles/main/.config/copyparty/windows.md" "%dir%\windows.md"
 
+call :download "https://raw.githubusercontent.com/mau-mauricelim/dotfiles/main/.config/copyparty/icons/gemini_cloudparty.ico" "%dir%\icons\gemini_cloudparty.ico"
+call :download "https://raw.githubusercontent.com/mau-mauricelim/dotfiles/main/.config/copyparty/icons/gemini_copyparty.ico" "%dir%\icons\gemini_copyparty.ico"
+
 :: Runs copyparty from any directory
 call :download "https://github.com/9001/copyparty/releases/latest/download/copyparty-sfx.py" "%dir%\party.py"
 call :download "https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-windows-amd64.exe" "%dir%\cloudflared.exe"
 
 set confirm=n
-set /P confirm=Do you want to create desktop shortcuts? [Y/n] 
+set /P confirm=Do you want to create Desktop shortcuts? [Y/n] 
 echo.
 if /I "%confirm%" NEQ "Y" goto pathcheck
 
-:: TODO: Add desktop shortcuts icons
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\cloudparty.lnk'); $s.TargetPath='%dir%\cloudparty.bat'; $s.WorkingDirectory='%dir%'; $s.Save()"
-echo Created desktop shortcut for: cloudparty
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\cloudparty.lnk'); $s.TargetPath='%dir%\cloudparty.bat'; $s.WorkingDirectory='%dir%'; $s.IconLocation='%dir%\icons\gemini_cloudparty.ico'; $s.Save()"
+echo Created Desktop shortcut for: cloudparty
 echo.
-powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\party.lnk'); $s.TargetPath='%dir%\party.bat'; $s.WorkingDirectory='%dir%'; $s.Save()"
-echo Created desktop shortcut for: party
+powershell "$s=(New-Object -COM WScript.Shell).CreateShortcut('%userprofile%\Desktop\party.lnk'); $s.TargetPath='%dir%\party.bat'; $s.WorkingDirectory='%dir%'; $s.IconLocation='%dir%\icons\gemini_copyparty.ico'; $s.Save()"
+echo Created Desktop shortcut for: party
 echo.
 
 :pathcheck
