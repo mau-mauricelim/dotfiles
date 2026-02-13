b60451f70c6f0cd802836bd31a618f69a53ee543
Maurice Lim <mau.mauricelim@gmail.com>
2026-02-13 08:22:56 +0000

    Replace git clone depth with blob none

--- install.sh
+++ install.sh
@@ -98,7 +98,7 @@ common_user_install() {
     # Create the top level directories before stowing so that stow does not symlink from the top level
     mkdir -p "$HOME/.config/"{nvim,tmux,yazi,zsh,q} "$HOME/.vim"
     # Clone the dotfiles
-    [ -d "$HOME/dotfiles" ] || git clone -b "$BRANCH_NAME" --depth=10 https://github.com/mau-mauricelim/dotfiles.git "$HOME/dotfiles" >/dev/null
+    [ -d "$HOME/dotfiles" ] || git clone -b "$BRANCH_NAME" --filter=blob:none https://github.com/mau-mauricelim/dotfiles.git "$HOME/dotfiles" >/dev/null
     # Copy the q folder as static files
     cp -r "$HOME/dotfiles/q" "$HOME"
     # Stow the dotfiles
