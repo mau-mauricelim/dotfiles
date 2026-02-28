f61e6293cf92b2c8b2b7aa039cc552268dc35d85
Maurice Lim <mau.mauricelim@gmail.com>
2026-02-28 06:09:58 +0000

    Fix: add support for new LSP config API in Neovim 0.11+

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -60,26 +60,13 @@ return { -- LSP Configuration & Plugins
         --  See `:help K` for why this keymap
         map('K', vim.lsp.buf.hover, 'Hover Documentation')
 
-        -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-        ---@param client vim.lsp.Client
-        ---@param method vim.lsp.protocol.Method
-        ---@param bufnr? integer some lsp support methods only in specific files
-        ---@return boolean
-        local function client_supports_method(client, method, bufnr)
-          if vim.fn.has 'nvim-0.11' == 1 then
-            return client:supports_method(method, bufnr)
-          else
-            return client.supports_method(method, { bufnr = bufnr })
-          end
-        end
-
         -- The following two autocommands are used to highlight references of the
         -- word under your cursor when your cursor rests there for a little while.
         --    See `:help CursorHold` for information about when this is executed
         --
         -- When you move your cursor, the highlights will be cleared (the second autocommand).
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+        if client and client:supports_method('textDocument/documentHighlight', event.buf) then
           local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
           vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
             buffer = event.buf,
@@ -104,33 +91,35 @@ return { -- LSP Configuration & Plugins
       end,
     })
 
-    -- LSP servers and clients are able to communicate to each other what features they support.
-    --  By default, Neovim doesn't support everything that is in the LSP specification.
-    --  When you add blink.cmp, luasnip, etc. Neovim now has *more* capabilities.
-    --  So, we create new capabilities with blink.cmp, and then broadcast that to the servers.
-    local capabilities = require('blink.cmp').get_lsp_capabilities()
-
     -- Enable the following language servers
     --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    --
-    --  Add any additional override configuration in the following tables. Available keys are:
-    --  - cmd (table): Override the default command used to start the server
-    --  - filetypes (table): Override the default list of associated filetypes for the server
-    --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-    --  - settings (table): Override the default settings passed when initializing the server.
-    --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+    --  See `:help lsp-config` for information about keys and how to configure
+    ---@type table<string, vim.lsp.Config>
     local servers = {
-      -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
-      --
-      -- Some languages (like typescript) have entire language plugins that can be useful:
-      --    https://github.com/pmizio/typescript-tools.nvim
-      --
-      -- But for many setups, the LSP (`pyright`) will work just fine
-      -- pyright = {},
+      stylua = {}, -- Used to format Lua code
       lua_ls = {
-        -- cmd = {...},
-        -- filetypes { ...},
-        -- capabilities = {},
+        on_init = function(client)
+          if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if path ~= vim.fn.stdpath 'config' and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc')) then return end
+          end
+
+          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+              version = 'LuaJIT',
+              path = { 'lua/?.lua', 'lua/?/init.lua' },
+            },
+            workspace = {
+              checkThirdParty = false,
+              -- NOTE: this is a lot slower and will cause issues when working on your own configuration.
+              --  See https://github.com/neovim/nvim-lspconfig/issues/3189
+              library = vim.tbl_extend('force', vim.api.nvim_get_runtime_file('', true), {
+                '${3rd}/luv/library',
+                '${3rd}/busted/library',
+              }),
+            },
+          })
+        end,
         settings = {
           Lua = {
             completion = {
@@ -145,63 +134,36 @@ return { -- LSP Configuration & Plugins
       },
     }
 
-    -- Disable LSP progress notifications
-    -- BUG: Since mason-lspconfig breaking change in V2, lua_ls window.progressBar is not working
-    --      https://github.com/mason-org/mason-lspconfig.nvim/issues/545
-    vim.lsp.handlers["$/progress"] = function() end
-
     -- Ensure the servers and tools above are installed
-    --  To check the current status of installed tools and/or manually install
-    --  other tools, you can run
+    --
+    -- To check the current status of installed tools and/or manually install
+    -- other tools, you can run
     --    :Mason
     --
-    --  You can press `g?` for help in this menu
-    --
-    -- `mason` had to be setup earlier: to configure its options see the
-    -- `dependencies` table for `nvim-lspconfig` above.
-    --
-    -- You can add other tools here that you want Mason to install
-    -- for you, so that they are available from within Neovim.
-
+    -- You can press `g?` for help in this menu.
     -- BUG: Extract server names but exclude lua_ls (lua-language-server is manually installed below)
     local ensure_installed = vim.tbl_filter(function(server)
         return server ~= 'lua_ls'
     end, vim.tbl_keys(servers or {}))
 
     vim.list_extend(ensure_installed, {
-        -- you can turn off/on auto_update per tool
-        { 'bash-language-server', auto_update = true },
-        'shfmt',
-        'shellcheck',
-        'stylua', -- Used to format lua code
-    })
-    require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
-
-    -- BUG: lua-language-server does not have linux-x64-musl.tar.gz above 3.15.0
-    -- Install specific version via registry
-    local registry = require('mason-registry')
-    local pkg_name = 'lua-language-server'
-    -- Refresh registry before install
-    registry.refresh(function()
-      if not registry.is_installed(pkg_name) then
-        local pkg = registry.get_package(pkg_name)
-        pkg:install(IsMusl() and { version = '3.15.0' } or nil)
-      end
-    end)
-
-    require('mason-lspconfig').setup({
-      ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
-      automatic_enable = true,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for tsserver)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
+      -- you can turn off/on auto_update per tool
+      { 'bash-language-server', auto_update = true },
+      'shfmt',
+      'shellcheck',
+      {
+        'lua_ls',
+        -- BUG: lua-language-server no longer supports linux-x64-musl.tar.gz above 3.15.0
+        version = IsMusl() and '3.15.0' or nil,
+        auto_update = not IsMusl(),
       },
     })
+
+    require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
+
+    for name, server in pairs(servers) do
+      vim.lsp.config(name, server)
+      vim.lsp.enable(name)
+    end
   end,
 }
