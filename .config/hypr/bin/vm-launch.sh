3b9ffa9077ac9b8fad848c6b3ccad69edd5d5cc5
Maurice Lim <mau.mauricelim@gmail.com>
2026-04-01 11:49:23 +0800

    Fix workspace issue

---
+++ .config/hypr/bin/vm-launch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+VM_NAME="${1:-win11}"
+BASE_WS="${2:-5}"
+NUM_MON=$(hyprctl monitors -j | jq '. | length')
+
+virsh --connect qemu:///system start "$VM_NAME"
+virt-viewer --connect qemu:///system --wait --attach "$VM_NAME" &
+PID=$!
+
+for i in {1..10}; do
+    mapfile -t ADDRS < <(hyprctl clients -j | jq -r --arg n "$VM_NAME" --arg p "$PID" \
+        '[.[] | select(.title | test($n + " \\(\\d+\\)")) | select(.pid == ($p | tonumber))] | sort_by(.title) | .[].address')
+    if [[ ${#ADDRS[@]} -ge $NUM_MON ]]; then
+        for I in "${!ADDRS[@]}"; do
+            hyprctl dispatch movetoworkspacesilent "$((BASE_WS + I)),address:${ADDRS[$I]}"
+        done
+        duration=$SECONDS
+        echo "Total elapsed time: $duration seconds"
+        break
+    fi
+    sleep 1
+done
