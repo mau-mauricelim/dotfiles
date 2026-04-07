c9904886b3d4d1e8a2c700491ab9ca973ab68cf4
Maurice Lim <mau.mauricelim@gmail.com>
2026-04-07 21:25:00 +0800

    Update vm-launch

--- .config/hypr/bin/vm-launch.sh
+++ .config/hypr/bin/vm-launch.sh
@@ -2,11 +2,22 @@
 
 VM_NAME="${1:-win11}"
 BASE_WS="${2:-5}"
-NUM_MON=$(hyprctl monitors -j | jq '. | length')
+EXECUTE="$3"
 
-virsh --connect qemu:///system start "$VM_NAME" && sleep 5
+# Get current day (1=Mon, 7=Sun) and hour (00-23)
+DAY=$(date +%u)
+HOUR=$(date +%H)
+# Check if today is a weekday (1-5) and specific timing (e.g., 8 AM to 6 PM)
+if ! [[ "$DAY" -le 5 ]] && [[ "$HOUR" -ge 8 && "$HOUR" -lt 18 ]]; then
+    if ! [[ "$EXECUTE" == "now" ]]; then
+        exit 0
+    fi
+fi
+
+virsh --connect qemu:///system start "$VM_NAME" && sleep 10
 virt-viewer --connect qemu:///system --wait --attach "$VM_NAME" &
 PID=$!
+NUM_MON=$(hyprctl monitors -j | jq '. | length')
 
 for i in {1..30}; do
     mapfile -t ADDRS < <(hyprctl clients -j | jq -r --arg n "$VM_NAME" --arg p "$PID" \
