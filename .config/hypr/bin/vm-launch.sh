f0e7dc2aeaf6b0d93985e12a097e1775b35e72f0
Maurice Lim <mau.mauricelim@gmail.com>
2026-04-09 10:27:44 +0800

    Test to see if this resolves the issue
    virt-viewer starts with 2 monitors, but the sizing is always not scaled
    properly and display 1 is always empty

--- .config/hypr/bin/vm-launch.sh
+++ .config/hypr/bin/vm-launch.sh
@@ -8,21 +8,22 @@ EXECUTE="$3"
 DAY=$(date +%u)
 HOUR=$(date +%H)
 # Check if today is a weekday (1-5) and specific timing (e.g., 8 AM to 6 PM)
-if ! [[ "$DAY" -le 5 ]] && [[ "$HOUR" -ge 8 && "$HOUR" -lt 18 ]]; then
-    if ! [[ "$EXECUTE" == "now" ]]; then
-        exit 0
-    fi
+if ! ([[ "$DAY" -le 5 ]] && [[ "$HOUR" -ge 8 && "$HOUR" -lt 18 ]]);  then
+    [[ "$EXECUTE" != "now" ]] && exit 0
 fi
 
-virsh --connect qemu:///system start "$VM_NAME" && sleep 10
+virsh --connect qemu:///system start "$VM_NAME" && sleep 5
 virt-viewer --connect qemu:///system --wait --attach "$VM_NAME" &
 PID=$!
+
 NUM_MON=$(hyprctl monitors -j | jq '. | length')
 
 for i in {1..30}; do
     mapfile -t ADDRS < <(hyprctl clients -j | jq -r --arg n "$VM_NAME" --arg p "$PID" \
         '[.[] | select(.title | test($n + " \\(\\d+\\)")) | select(.pid == ($p | tonumber))] | sort_by(.title) | .[].address')
     if [[ ${#ADDRS[@]} -ge $NUM_MON ]]; then
+        virsh --connect qemu:///system send-key "$VM_NAME" KEY_ESC
+        sleep 3
         for I in "${!ADDRS[@]}"; do
             hyprctl dispatch movetoworkspacesilent "$((BASE_WS + I)),address:${ADDRS[$I]}"
         done
@@ -30,5 +31,9 @@ for i in {1..30}; do
         echo "Total elapsed time: $duration seconds"
         break
     fi
+    # Optional: Nudge the VM every 10 seconds if windows haven't appeared
+    # if (( i % 10 == 0 )); then
+    #     virsh --connect qemu:///system send-key "$VM_NAME" KEY_LEFTCTRL
+    # fi
     sleep 1
 done
