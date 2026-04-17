9301d4b562bc383cb067aaa046f200bd9572e7ed
Maurice Lim <mau.mauricelim@gmail.com>
2026-04-17 09:49:18 +0800

    Update script

--- .config/hypr/bin/vm-launch.sh
+++ .config/hypr/bin/vm-launch.sh
@@ -4,15 +4,15 @@ VM_NAME="${1:-win11}"
 BASE_WS="${2:-5}"
 EXECUTE="$3"
 
-# Get current day (1=Mon, 7=Sun) and hour (00-23)
+# Get current day (1=Mon, 7=Sun) and hour (0-23)
 DAY=$(date +%u)
-HOUR=$(date +%H)
+HOUR=$(date +%-H)
 # Check if today is a weekday (1-5) and specific timing (e.g., 8 AM to 6 PM)
 if ! ([[ "$DAY" -le 5 ]] && [[ "$HOUR" -ge 8 && "$HOUR" -lt 18 ]]);  then
     [[ "$EXECUTE" != "now" ]] && exit 0
 fi
 
-virsh --connect qemu:///system start "$VM_NAME" && sleep 5
+virsh --connect qemu:///system start "$VM_NAME" && sleep 10
 virt-viewer --connect qemu:///system --wait --attach "$VM_NAME" &
 PID=$!
 
